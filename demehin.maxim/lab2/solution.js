99ec5bb0fa8aa48473f855fd8bdce997172a6513
Demehin Maxim <maxdem06@gmail.com>
2026-09-14 12:29:10 +0300

    add toRoman func

--- demehin.maxim/lab2/solution.js
+++ demehin.maxim/lab2/solution.js
@@ -1,2 +1,30 @@
-// Экспортируйте отсюда функцию с именем из контракта вашего варианта.
-export {};
+export function toRoman(num) {
+  if (!Number.isInteger(num) || num < 1 || num > 3999) {
+    throw new RangeError("Invalid number");
+  }
+
+  const map = [
+    [1000, "M"],
+    [900, "CM"],
+    [500, "D"],
+    [400, "CD"],
+    [100, "C"],
+    [90, "XC"],
+    [50, "L"],
+    [40, "XL"],
+    [10, "X"],
+    [9, "IX"],
+    [5, "V"],
+    [4, "IV"],
+    [1, "I"],
+  ];
+
+  let result = "";
+  for (const [value, symbol] of map) {
+    while (num >= value) {
+      result += symbol;
+      num -= value;
+    }
+  }
+  return result;
+}
