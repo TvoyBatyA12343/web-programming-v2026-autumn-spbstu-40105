934e6b0d43c0e326810df08bc5a765e2aad7321f
Demehin Maxim <maxdem06@gmail.com>
2026-09-14 12:43:50 +0300

    prettier fix

--- demehin.maxim/lab2/solution.js
+++ demehin.maxim/lab2/solution.js
@@ -1,25 +1,25 @@
 export function toRoman(num) {
   if (!Number.isInteger(num) || num < 1 || num > 3999) {
-    throw new RangeError("Invalid number");
+    throw new RangeError('Invalid number');
   }
 
   const map = [
-    [1000, "M"],
-    [900, "CM"],
-    [500, "D"],
-    [400, "CD"],
-    [100, "C"],
-    [90, "XC"],
-    [50, "L"],
-    [40, "XL"],
-    [10, "X"],
-    [9, "IX"],
-    [5, "V"],
-    [4, "IV"],
-    [1, "I"],
+    [1000, 'M'],
+    [900, 'CM'],
+    [500, 'D'],
+    [400, 'CD'],
+    [100, 'C'],
+    [90, 'XC'],
+    [50, 'L'],
+    [40, 'XL'],
+    [10, 'X'],
+    [9, 'IX'],
+    [5, 'V'],
+    [4, 'IV'],
+    [1, 'I'],
   ];
 
-  let result = "";
+  let result = '';
   for (const [value, symbol] of map) {
     while (num >= value) {
       result += symbol;
