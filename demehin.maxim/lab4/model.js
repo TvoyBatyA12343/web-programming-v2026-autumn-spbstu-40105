e25985c3d19f2f1d29dcc1f70cd3c7937b144765
Demehin Maxim <maxdem06@gmail.com>
2026-09-21 11:01:26 +0300

    student class impl

--- demehin.maxim/lab4/model.js
+++ demehin.maxim/lab4/model.js
@@ -1,2 +1,29 @@
-// Экспортируйте класс и функции коллекций с именами из контракта вашего варианта.
-export {};
+export class Student {
+  constructor(id, name, grades = {}) {
+    this.id = id;
+    this.name = name;
+    this.grades = grades;
+  }
+
+  addGrade(subject, grade) {
+    this.grades[subject] = grade;
+  }
+
+  removeGrade(subject) {
+    delete this.grades[subject];
+  }
+
+  getAverageGrade() {
+    const values = Object.values(this.grades);
+    if (values.length === 0) {
+        return 0;
+    }
+    
+    const sum = values.reduce((acc, g) => acc + g, 0);
+    return sum / values.length;
+  }
+
+    get summary() {
+    return `Студент ${this.name} (id: ${this.id}) — средний балл: ${this.getAverageGrade().toFixed(2)}`;
+  }
+}
