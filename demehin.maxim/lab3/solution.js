5508f25911bb84ce660b4edeb4b148d0ef2da668
Demehin Maxim <maxdem06@gmail.com>
2026-09-21 10:48:38 +0300

    rotateArray impl

--- demehin.maxim/lab3/solution.js
+++ demehin.maxim/lab3/solution.js
@@ -1,1 +1,14 @@
-export {};
+export function rotateArray(arr, steps) {
+  if (!Number.isInteger(steps)) {
+    throw new TypeError('second arg should be an integer');
+  }
+
+  const n = arr.length;
+  if (n === 0) {
+    return [];
+  }
+
+  const k = ((steps % n) + n) % n;
+
+  return [...arr.slice(n - k), ...arr.slice(0, n - k)];
+}
